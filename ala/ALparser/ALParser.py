67a9a029dc8d251eecf3f0f14f38ed103ca59abc
Jakub Semczyszyn <kubase.se@gmail.com>
2021-11-19 17:52:52 +0100

    Parser pattern matching refactor

--- ala/ALparser/ALParser.py
+++ ala/ALparser/ALParser.py
@@ -2,22 +2,31 @@ import re
 import pandas as pd
 
 class ApacheLogParser:
-    logLineRegex = '([(\d\.)]+) ([\w-]+) ([\w-]+) \[(.*?)\] "(.*?)" (\d+) ([\d-]+) "(.*?)" "(.*?)"\n?'
+    NCSAExtendedCombinedLogFormatRegex = '([(\d\.)]+) "?([\w-]+)"? "?([\w-]+)"? \[(.*?)\] "(.*?)" (\d+) ([\d-]+) "(.*?)" "(.*?)"\n?'
     __logs = []
 
     def parseLine(self, logLine):
-        matched = re.fullmatch(self.logLineRegex, logLine)
+        matched = re.fullmatch(self.NCSAExtendedCombinedLogFormatRegex, logLine)
         if matched:
-            if isinstance(self.__logs, pd.DataFrame):
-                self.__logs = self.__logs.append({'raw': logLine, 'host': matched[1], 'logname': matched[2], 'user': matched[3], 'time': matched[4], 'request': matched[5], 'status': matched[6], 'bytes_of_response': matched[7], 'referer': matched[8], 'user_agent': matched[9]},ignore_index=True)
+            if self.areLogsDF():
+                self.addLogLineToDF(matched, logLine)
             else:
-                self.__logs.append({'raw': logLine, 'host': matched[1], 'logname': matched[2], 'user': matched[3], 'time': matched[4], 'request': matched[5], 'status': matched[6], 'bytes_of_response': matched[7], 'referer': matched[8], 'user_agent': matched[9]})
+                self.addLogLineToList(matched, logLine)
 
     def process(self):
         self.__logs = pd.DataFrame(self.__logs)
         self.normalize()
         self.categorize()
         self.enrich()
+
+    def areLogsDF(self):
+        return isinstance(self.__logs, pd.DataFrame)
+
+    def addLogLineToDF(self, matched, line):
+        self.__logs = self.__logs.append({'raw': line, 'host': matched[1], 'logname': matched[2], 'user': matched[3], 'time': matched[4], 'request': matched[5], 'status': matched[6], 'bytes_of_response': matched[7], 'referer': matched[8], 'user_agent': matched[9]},ignore_index=True)
+    
+    def addLogLineToList(self, matched, line):
+        self.__logs.append({'raw': line, 'host': matched[1], 'logname': matched[2], 'user': matched[3], 'time': matched[4], 'request': matched[5], 'status': matched[6], 'bytes_of_response': matched[7], 'referer': matched[8], 'user_agent': matched[9]})
     
     def normalize(self):
         pass
